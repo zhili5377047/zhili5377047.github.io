6acb81f2c2259ac4daefd8d67d8e4d8066dd60c0
zhili5377047 <1326702376@qq.com>
2016-11-19 21:11:38 +0800

    Site updated: 2016-11-19 21:11:38

--- single/homefamily(single)/js/login.js
+++
@@ -1,4 +0,0 @@
-//登录部分
-		var oInput = document.querySelectorAll(".input");
-		var oA = document.querySelectorAll(".input a");
-		for(var i=0;i
--- single/homeliving(pc)/js/about_us/about_activation_1.js
+++
@@ -1,9 +0,0 @@
-$(".section .aside li").on('click',function(){
-	var index = $(this).index();
-	$(this).find("a").addClass("change_color");
-	$(this).siblings().find("a").removeClass("change_color");
-	$(this).parents("ul").siblings("div").find(".circle").removeClass("change_bg");
-	$(this).parents("ul").siblings("div").find(".circle").eq(index).addClass("change_bg");
-	$(".section .main").hide();
-	$(".section .main").eq(index).show();
-})
